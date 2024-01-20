61184d2a2fe2610c18c6f9f274232be748ed4c51
Thales Ferreira <thaleszim@live.com>
2024-01-20 20:36:53 -0300

    adicionar

---
+++ Exercicio.3_4.py
@@ -0,0 +1,12 @@
+A = 112323
+B = 231132
+C = 323211
+
+if A <= B  and A <= C:
+    if B <= C:
+        print("Ordem Crescente: {}, {}, {}".format(A,B,C))
+
+elif B <= C:
+    print("Ordem Crescente: {}, {}, {}".format(A,B,C))
+
+
